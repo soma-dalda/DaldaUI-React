9f34cdd5938c4addcb357789b2692d70963f5eaf
jaewoong2 <jwisgenius@naver.com>
2022-07-23 17:36:24 +0900

    :lipstick: Typography 설정

--- src/styles/theme/constant.ts
+++ src/styles/theme/constant.ts
@@ -1,7 +1,5 @@
 export const color = {}
 
 export const breakpoint = {
-  mobile: '700px',
   maxWidth: '1024px',
-  desktop: '1268px',
 }
--- src/styles/theme/emotion.d.ts
+++ src/styles/theme/emotion.d.ts
@@ -1,8 +1,9 @@
-import { color } from './constant'
+import { color, breakpoint } from './constant'
 import '@emotion/react'
 
 declare module '@emotion/react' {
   export interface Theme {
     color: typeof color
+    breakpoint: typeof breakpoint
   }
 }
