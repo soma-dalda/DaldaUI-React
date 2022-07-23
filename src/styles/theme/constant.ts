25b2df08c5e2107f467ac0790290ea3561eb7228
jaewoong2 <jwisgenius@naver.com>
2022-07-23 17:49:06 +0900

    :lipstick: semantic color 구현

--- src/styles/theme/constant.ts
+++ src/styles/theme/constant.ts
@@ -1,5 +1,25 @@
-export const color = {}
+export const color = {
+  // Background Color
+  searchInputBG: '#FBF7F2',
+  resultInfoBG: '#FBF7F2',
+  buttonPrimaryBG: '#FFA180',
+  buttonNormalBG: '#FFFFFF',
+  // Text Color
+  primaryText: '#0C73AC',
+  smallText: '#696969',
+  // Border Color
+  buttonBorderColor: '#BDBDBD',
+}
 
 export const breakpoint = {
   maxWidth: '1024px',
+}
+
+export const borderRaidus = {
+  searchInput: '16px',
+  resultInfo: '14px',
+  bottomSheet: '30px 30px 0 0',
+  button: '14px',
+  listItem: '20px',
+  listImageItem: '20px 20px 0 0',
 }
